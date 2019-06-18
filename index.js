da0f99b34f79a89ccb2698e01e72a9f2e964025f
Tim Hagn <mail@timhagn.com>
2019-06-18 13:29:40 +0200

    move resolvers & schemas to folder lib

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const cors = require('micro-cors')(); // highlight-line
-const { ApolloServer, gql } = require('apollo-server-micro');
+const cors = require('micro-cors')()
+const { ApolloServer } = require('apollo-server-micro')
 
-const typeDefs = gql`
-    type Query {
-        sayHello: String
-    }
-`;
-
-const resolvers = {
-  Query: {
-    sayHello(parent, args, context) {
-      return 'Hello World!';
-    },
-  },
-};
+const typeDefs = require(`./lib/schemas`)
+const resolvers = require(`./lib/resolvers`)
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 module.exports = cors(apolloServer.createHandler());
