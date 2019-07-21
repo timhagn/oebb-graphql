9d62e2b4dbae52adf719f002909de0174c95b069
Tim Hagn <mail@timhagn.com>
2019-07-21 20:07:46 +0200

    update deps, push test coverage to 100%

--- src/datasources/__tests__/OebbAPI.js
+++ src/datasources/__tests__/OebbAPI.js
@@ -1,4 +1,5 @@
 const OebbAPI = require(`../OebbAPI`)
+const { isString } = require('../../utils')
 
 // We have to up jest's timeout as HAFAS might be busy.
 jest.setTimeout(10000)
@@ -6,6 +7,11 @@ jest.setTimeout(10000)
 const oebbAPI = new OebbAPI()
 
 describe(`[OebbAPI].isHealthy`, () => {
+  it(`initialize() sets this.context`, async () => {
+    oebbAPI.initialize({ context: `test` })
+    expect(oebbAPI.context).toEqual(`test`)
+  })
+
   it(`isHealthy() returns true`, async () => {
     const healthy = await oebbAPI.isHealthy()
     expect(healthy).toBeTruthy()
@@ -57,5 +63,86 @@ describe(`[OebbAPI].getJourneys`, () => {
     expect(journeys).toHaveProperty(`earlierRef`)
     expect(journeys).toHaveProperty(`laterRef`)
     expect(journeys).toHaveProperty(`journeys`)
+  })
+})
+
+describe(`[OebbAPI].legReducer`, () => {
+  const dummyLocation = {
+    id: `test`,
+    name: `test`,
+    type: `test`,
+    location: `test`,
+    products: `test`,
+  }
+
+  const dummyOperator = {
+    type: `test`,
+    id: `test`,
+    name: `test`,
+  }
+
+  const dummyLine = {
+    type: `test`,
+    id: `test`,
+    fahrtNr: `test`,
+    name: `test`,
+    public: `test`,
+    mode: `test`,
+    product: `test`,
+    operator: dummyOperator,
+  }
+
+  const dummyLeg = {
+    origin: dummyLocation,
+    destination: dummyLocation,
+    departure: `test`,
+    arrival: `test`,
+    reachable: `test`,
+    tripId: `test`,
+    line: dummyLine,
+    walking: true,
+    distance: 10,
+    direction: `test`,
+    arrivalPlatform: `test`,
+    departurePlatform: `test`,
+  }
+
+  it(`legReducer() returns a "reduced" leg`, async () => {
+    const leg = oebbAPI.legReducer(dummyLeg)
+    expect(leg).toMatchSnapshot()
+    delete dummyLeg.line
+    const legWithoutLine = oebbAPI.legReducer(dummyLeg)
+    expect(legWithoutLine.line).toEqual({})
+  })
+})
+
+describe(`mocked HAFAS for [OebbAPI].getStopInfo & [OebbAPI].getLocations`, () => {
+  beforeEach(() => {
+    oebbAPI.hafasClient.locations = jest.fn(async () => {})
+    oebbAPI.hafasClient.stop = jest.fn(async () => [])
+  })
+
+  it(`getStopInfo() returns empty object`, async () => {
+    const wienHbf = `8101590`
+    const locations = await oebbAPI.getStopInfo(wienHbf)
+    expect(locations).toMatchInlineSnapshot(`Object {}`)
+  })
+
+  it(`getStopInfo() returns object for id`, async () => {
+    const name = `Wien`
+    const stop = await oebbAPI.getLocations(name)
+    expect(stop).toMatchInlineSnapshot(`Array []`)
+  })
+})
+
+describe(`isString() util`, () => {
+  it(`isString() returns true for string`, () => {
+    const testString = isString(`test`)
+    expect(testString).toBeTruthy()
+  })
+
+  it(`isString() returns false for non string`, () => {
+    const testString = isString({})
+    expect(testString).toBeFalsy()
   })
 })
